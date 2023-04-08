4105aecab1100eff1fc2b0779f2a8d12a8bf4caa
Sebastian Vargas <sebastian.vargas@reign.cl>
2023-04-08 15:09:44 -0500

    feat: create auth context

---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,12 @@
+import { types } from "../types/types";
+
+export function authReducer(state = {}, action) {
+  switch (action.type) {
+    case types.login:
+      return { ...state, logged: true, name: action.payload };
+    case types.logout:
+      return { ...state, logged: false };
+    default:
+      return state;
+  }
+}
